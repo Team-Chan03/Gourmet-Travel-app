e42c55cbed32463d26be2c7c11d22c9e3ad82c9f
Hideaki-Umezawa <hideaki_umezawa@mail.toyota.co.jp>
2025-05-28 01:06:03 +0900

    client/src/App.jsxにてuser現在地取得できるかテスト

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    const { latitude, longitude } = position.coords;
+    console.log(
+      "🚀 ~ navigator.geolocation.getCurrentPosition ~ latitude:",
+      latitude
+    );
+    console.log(
+      "🚀 ~ navigator.geolocation.getCurrentPosition ~ longitude:",
+      longitude
+    );
+
+    const res = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+    );
+    const data = await res.json();
+
+    const province = data.address.province;
+    console.log(
+      "🚀 ~ navigator.geolocation.getCurrentPosition ~ province:",
+      province
+    );
+  });
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +49,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
