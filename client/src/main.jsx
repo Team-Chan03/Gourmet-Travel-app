ceebd06bbaf737510e1dfe9effa0e22e9329dcb8
Tsuruta-Yuki <yuki_tsuruta_aa@mail.toyota.co.jp>
2025-05-28 14:04:39 +0900

    簡易Mypage作成&ルーター設定

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import LoginForm from './components/LoginForm.jsx';
 import RegisterForm from './components/RegisterForm.jsx';
+import Mypage from './components/Mypage.jsx';
 import { BrowserRouter, Route, Navigate, Routes } from 'react-router';
 
 // import './index.css'
@@ -13,7 +14,11 @@ createRoot(document.getElementById('root')).render(
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
-      <Route path="/form" element={<App />} />
+      <Route path="/records" element={<App />} />
+      <Route path="/mypage" element={<Mypage />} />
+      <Route path="/mypage/records" element={<Myrecords />} />
+      <Route path="/mypage/map" element={<Map />} />
+      <Route path="/mypage/stamp" element={<Stamp />} />
     </Routes>
   </BrowserRouter>
 );
