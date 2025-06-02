a54ff14d27d53f6664c5ce628ce62399958a32b6
Wentz <ryo_uematsu@mail.toyota.co.jp>
2025-06-02 23:19:03 +0900

    map2不要な行削除

--- client/src/features/records/map/JapanMap.jsx
+++ client/src/features/records/map/JapanMap.jsx
@@ -4,156 +4,11 @@ import geoJson from "./../../../utils/japan.json";
 
 const getTarget = ({ list, prefName }) => {
   let pref = prefName;
-  // switch (prefName) {
-  //   case "Kagoshima":
-  //     pref = "鹿児島県";
-  //     break;
-  //   case "Oita":
-  //     pref = "大分県";
-  //     break;
-  //   case "Fukuoka":
-  //     pref = "福岡県";
-  //     break;
-  //   case "Saga":
-  //     pref = "佐賀県";
-  //     break;
-  //   case "Nagasaki":
-  //     pref = "長崎県";
-  //     break;
-  //   case "Kumamoto":
-  //     pref = "熊本県";
-  //     break;
-  //   case "Miyazaki":
-  //     pref = "宮崎県";
-  //     break;
-  //   case "Tokushima":
-  //     pref = "徳島県";
-  //     break;
-  //   case "Kagawa":
-  //     pref = "香川県";
-  //     break;
-  //   case "Ehime":
-  //     pref = "愛媛県";
-  //     break;
-  //   case "Kochi":
-  //     pref = "高知県";
-  //     break;
-  //   case "Shimane":
-  //     pref = "島根県";
-  //     break;
-  //   case "Yamaguchi":
-  //     pref = "山口県";
-  //     break;
-  //   case "Tottori":
-  //     pref = "鳥取県";
-  //     break;
-  //   case "Hyogo":
-  //     pref = "兵庫県";
-  //     break;
-  //   case "Kyoto":
-  //     pref = "京都府";
-  //     break;
-  //   case "Fukui":
-  //     pref = "福井県";
-  //     break;
-  //   case "Ishikawa":
-  //     pref = "石川県";
-  //     break;
-  //   case "Toyama":
-  //     pref = "富山県";
-  //     break;
-  //   case "Niigata":
-  //     pref = "新潟県";
-  //     break;
-  //   case "Yamagata":
-  //     pref = "山形県";
-  //     break;
-  //   case "Akita":
-  //     pref = "秋田県";
-  //     break;
-  //   case "Aomori":
-  //     pref = "青森県";
-  //     break;
-  //   case "Iwate":
-  //     pref = "岩手県";
-  //     break;
-  //   case "Miyagi":
-  //     pref = "宮城県";
-  //     break;
-  //   case "Fukushima":
-  //     pref = "福島県";
-  //     break;
-  //   case "Ibaraki":
-  //     pref = "茨城県";
-  //     break;
-  //   case "Chiba":
-  //     pref = "千葉県";
-  //     break;
-  //   case "Tokyo":
-  //     pref = "東京都";
-  //     break;
-  //   case "Kanagawa":
-  //     pref = "神奈川県";
-  //     break;
-  //   case "Shizuoka":
-  //     pref = "静岡県";
-  //     break;
-  //   case "Aichi":
-  //     pref = "愛知県";
-  //     break;
-  //   case "Mie":
-  //     pref = "三重県";
-  //     break;
-  //   case "Wakayama":
-  //     pref = "和歌山県";
-  //     break;
-  //   case "Osaka":
-  //     pref = "大阪府";
-  //     break;
-  //   case "Okayama":
-  //     pref = "岡山県";
-  //     break;
-  //   case "Hiroshima":
-  //     pref = "広島県";
-  //     break;
-  //   case "Hokkaido":
-  //     pref = "北海道";
-  //     break;
-  //   case "Okinawa":
-  //     pref = "沖縄県";
-  //     break;
-  //   case "Gunma":
-  //     pref = "群馬県";
-  //     break;
-  //   case "Nagano":
-  //     pref = "長野県";
-  //     break;
-  //   case "Tochigi":
-  //     pref = "栃木県";
-  //     break;
-  //   case "Gifu":
-  //     pref = "岐阜県";
-  //     break;
-  //   case "Shiga":
-  //     pref = "滋賀県";
-  //     break;
-  //   case "Saitama":
-  //     pref = "埼玉県";
-  //     break;
-  //   case "Yamanashi":
-  //     pref = "山梨県";
-  //     break;
-  //   case "Nara":
-  //     pref = "奈良県";
-  //     break;
-  // }
-
   let count = 0;
   list.map((e) => {
     if (e.region === pref){
        count ++;
       }
-    // if (e.name === pref) count = e;
   })
   if(count>0)console.log("count",pref, count);
   
@@ -232,12 +87,6 @@ const JapanMap = ({ list }) => {
         if (!t || t.count === 0) return ;
         return "pointer";
       })
-      // .attr(`fill-opacity`, (item) => {
-      //   // 透明度の設定
-      //   const t = getTarget({ list, prefName: item.properties.name_ja });
-      //   if (!t || t.count === 0) return 0;
-      //   return t * 0.5;
-      // })
 
       /**
        * 都道府県領域の click イベントハンドラ
