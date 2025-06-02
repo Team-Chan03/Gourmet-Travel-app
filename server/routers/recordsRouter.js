337916798d1525705c0c7b59197de6f68a2b7f03
yuta_sato <yuta_sato@mail.toyota.co.jp>
2025-06-02 11:24:46 +0900

    building

--- server/routers/recordsRouter.js
+++ server/routers/recordsRouter.js
@@ -1,24 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const db = require("../db");
+const db = require('../db');
 // import axios from "axios";
 
-
 //wentz:自分の投稿以外も取ってくるAPI
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const list = await db("records").select("*").orderBy("created_at", "desc");
+    const list = await db('records').select('*').orderBy('created_at', 'desc');
     res.status(200).json(list);
   } catch (err) {
-    console.error("🔥 /api/records error:", err.message);
+    console.error('🔥 /api/records error:', err.message);
     res.status(500).json({ error: err.message });
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   //緯度経度ここで取得してテーブルにインサートする
-  console.log("このデータを今後インサートしていく予定", req.body);
-  const { latitude, longitude, user_id, rating, created_at } = req.body;
+  console.log('このデータを今後インサートしていく予定', req.body);
+  const { latitude, longitude, user_id, rating, created_at, comment, image_url } = req.body;
 
   const resMap = await fetch(
     `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
@@ -26,24 +25,34 @@ router.post("/", async (req, res) => {
 
   const data = await resMap.json();
   //wentz:provinceに県名が入る
-  const province = data.address.province;
+  const region = data.address.province;
 
   console.log(
     `🚀 ~ router.post ~   {
     latitude, longitude, user_id, stamp_num, created_at, province;
   }:`,
     {
+      image_url,
       latitude,
       longitude,
       user_id,
       rating,
+      region,
+      comment,
       created_at,
-      province,
     }
   );
+
+  await db('records').insert({
+    latitude,
+    longitude,
+    user_id,
+    rating,
+    created_at,
+    province: region,
+  });
 
   res.json(req.body);
 });
 
 module.exports = router;
-
